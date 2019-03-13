b3ed6ef13f5c3b31b82264761185ad9534245f15
Peter Ramos <peterdramos02@gmail.com>
2019-03-13 17:34:34 -0400

    add mob class and spawn them

--- smup.py
+++ smup.py
@@ -43,10 +43,40 @@ class Player(pygame.sprite.Sprite):
         if self.rect.left < 0:
             self.rect.left = 0
 	  
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 40))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        # The 0 as the first param in randrange is optional. If you 
+        # leave it out, pygame will know that it's supposed to be 0
+        self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40 - self.rect.height)
+        self.speedy = random.randrange(1, 8)
+        self.speedx = random.randrange(-3, 3)
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        # If the mob sprite ever goes off the bottom of the screen (ie: the player missed it),
+        # We will move it back to the top but randomize its position and speed again
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
+            self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40 - self.rect.height)
+            self.speedy = random.randrange(1, 8)
+
+
+
 
 all_sprites = pygame.sprite.Group()
+mobs = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
+for i in range(8):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
 # Game loop
 running = True
 while running:
